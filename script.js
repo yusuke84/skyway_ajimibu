cfb00bfc6958508c91e46257bee470ba669dabdc
Y-NAKA <com.y.naka@gmail.com>
2013-12-20 16:30:50 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,116 @@
+/**
+ * Created by yusuke on 2013/12/20.
+ */
+
+//APIキー
+var APIKEY = '6165842a-5c0d-11e3-b514-75d3313b9d05';
+
+//ユーザーリスト
+var userList = [];
+
+//Callオブジェクト
+var existingCall;
+
+// Compatibility shim
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+// PeerJSオブジェクトを生成
+var peer = new Peer({ key: APIKEY, debug: 3});
+
+// PeerIDを生成
+peer.on('open', function(){
+    $('#my-id').text(peer.id);
+});
+
+// 相手からのコールを受信する
+peer.on('call', function(call){
+    // 自身のメディアストリームをセットしてアンサーを返す
+    call.answer(window.localStream);
+    step3(call);
+});
+
+// エラーハンドラ
+peer.on('error', function(err){
+    alert(err.message);
+    step2();
+});
+
+// イベントハンドラー
+$(function(){
+
+    $('#make-call').click(function(){
+        // 相手に接続
+        var call = peer.call($('#contactlist').val(), window.localStream);
+        step3(call);
+
+    });
+
+    $('#end-call').click(function(){
+        existingCall.close();
+        step2();
+    });
+
+    // メディアストリームを再取得
+    $('#step1-retry').click(function(){
+        $('#step1-error').hide();
+        step1();
+    });
+
+    // ステップ１実行
+    step1();
+
+    //ユーザリス取得開始
+    setInterval(getUserList, 2000);
+
+});
+
+function step1 () {
+    // メディアストリームを取得する
+    navigator.getUserMedia({audio: true, video: true}, function(stream){
+        // 自分のVIDEOタグにメディアストリームをセットする
+        $('#my-video').prop('src', URL.createObjectURL(stream));
+        window.localStream = stream;
+        step2();
+    }, function(){ $('#step1-error').show(); });
+}
+
+function step2 () {
+    //UIコントロール
+    $('#step1, #step3').hide();
+    $('#step2').show();
+}
+
+function step3 (call) {
+    // すでに接続中の場合はクローズする
+    if (existingCall) {
+        existingCall.close();
+    }
+
+    // 相手からのメディアストリームを待ち受ける
+    call.on('stream', function(stream){
+        $('#their-video').prop('src', URL.createObjectURL(stream));
+    });
+
+    // 相手がクローズした場合
+    call.on('close', step2);
+
+    existingCall = call;
+    $('#their-id').text(call.peer);
+    $('#step1, #step2').hide();
+    $('#step3').show();
+
+}
+
+function getUserList () {
+    $.get('https://skyway.io/active/list/'+APIKEY,
+        function(list){
+            for(var cnt = 0;cnt < list.length;cnt++){
+                if($.inArray(list[cnt],userList)<0 && list[cnt] != peer.id){
+                    userList.push(list[cnt]);
+                    $('#contactlist').append($('<option>', {"value":list[cnt],"text":list[cnt]}));
+                }
+            }
+        }
+    );
+}
+
